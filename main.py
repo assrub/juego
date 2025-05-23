676b87f894ae4299f0667812a06a4767b0fde86b
Eduardo Martinez <eduardomartinezjuan23@gmail.com>
2025-05-22 22:10:40 -0300

    Proyecto

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
+from util import TAM_PANTALLA_X, TAM_PANTALLA_Y
 
 #INSTANCIAS
 pygame.init()
 
 #VARIABLES
-pantalla = pygame.display.set_mode((500, 500))
+pantalla = pygame.display.set_mode((TAM_PANTALLA_X, TAM_PANTALLA_Y))
 fondo = pygame.image.load("./assets/Imagenes/fondo.jpg").convert()
-fondo = pygame.transform.scale(fondo, (500, 500))
+fondo = pygame.transform.scale(fondo, (TAM_PANTALLA_X, TAM_PANTALLA_Y))
 pygame.display.set_caption("J23 - OOP")
 reloj = pygame.time.Clock()
 
